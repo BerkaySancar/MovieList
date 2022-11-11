f82a7838df6ed9a6f0515f34bacfa9fb5991ca02
Berkay Sancar <sancarberkay@gmail.com>
2022-11-11 10:37:36 +0300

    add swiftlint&alamofire pods

---
+++ MovieList/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  MovieList
+//
+//  Created by Berkay Sancar on 11.11.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowsScene = (scene as? UIWindowScene) else { return }
+        
+    }
+}
